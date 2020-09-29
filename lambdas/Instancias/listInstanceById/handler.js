54a8b2735754e4dd11664bc15d50f73148a62bdb
Nicolás Améstica Vidal <niamesvi3@gmail.com>
2020-09-29 02:55:03 -0300

    Se actualiza función listInstanceById

--- lambdas/Instancias/listInstanceById/handler.js
+++ lambdas/Instancias/listInstanceById/handler.js
@@ -20,8 +20,22 @@ module.exports.generico = async (event) => {
             return Responses._404({ error: 'No existen instancias.' });
         }
 
+        let instancia = {
+            InstanceId: data.Reservations[0].Instances[0].InstanceId,
+            InstanceType: data.Reservations[0].Instances[0].InstanceType,
+            Monitoring: data.Reservations[0].Instances[0].Monitoring.State,
+            PrivateDnsName: data.Reservations[0].Instances[0].PrivateDnsName,
+            PrivateIpAddress: data.Reservations[0].Instances[0].PrivateIpAddress,
+            PublicDnsName: data.Reservations[0].Instances[0].PublicDnsName,
+            State: data.Reservations[0].Instances[0].State,
+            SubnetId: data.Reservations[0].Instances[0].SubnetId,
+            VpcId: data.Reservations[0].Instances[0].VpcId,
+            SecurityGroups: data.Reservations[0].Instances[0].SecurityGroups,
+            Tags: data.Reservations[0].Instances[0].Tags,
+        }
+
         // RETORNA RESPUESTA
-        return Responses._200({ message: `Instancias listadas correctamente.`, "data" : data });
+        return Responses._200({ message: `Instancia listada correctamente.`, "data" : instancia });
     } catch (err) {
         console.log(err);
         return Responses._500({ error: 'No se ha podido acceder al servicio.', err });
